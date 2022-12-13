8287a8b8c4ebc879af7c29bb70e1aad6357b98d0
EvaXausa <xausa.eva@gmail.com>
2022-12-13 19:14:50 +0100

    for

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,12 @@
+/*
+chiedere per 10 volte all’utente un numero. 
+stampare la somma di tutti i numeri inseriti.
+*/
 
+//per 10 volte chiedo all'utente un numero
+for (let i = 1; i < 11; i++) {
+  //chiedo all'utente un numero per 10 volte
+  const numbers = prompt("inserisci un numero");
+}
+let somma = 4 + 4;
+console.log("somma");
